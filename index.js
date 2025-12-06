197b26e7dcfccfc814a567aad9d37aa70abe415d
khushi Vishwakarma <khushivishwakarma94337@gmail.com>
2025-12-06 22:19:12 +0530

    Adding new changes such as form

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express=require("express");
 const app=express();
+app.use(express.static("public"));
 const port=3000;
+let Query=[];
+let nextid=0;
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
-    res.send("requeted");
+app.get("/form",(req,res)=>{
+    res.render("index.ejs");
+
+});
+
+app.post("/request",(req,res)=>{
+    let{name,location}=req.body;
+    Query.push({id:nextid++,
+        name:name,
+        location:location,
+        });
+    
+
+});
+app.get("/home/complaint",(req,res)=>{
+    res.render("complaint.ejs");
+    
+    
+});
+app.post("/add",(req,res)=>{    
+    let {username,query}=req.body;
+
+
+    query=Query.find((p)=> username===p.username);
+
+    Query.push({query});
+    res.render("dashboard.ejs",{Query});
 
 });
 app.listen(port,()=>{
